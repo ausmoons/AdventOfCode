bd18eb0ac89b8da94a389211ebb618a24922351b
Liva <ausmoons@gmail.com>
2021-12-05 17:47:58 +0200

    Day 1, 2

---
+++ CodeOfAdvent/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace CodeOfAdvent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:..\..\..\input1.txt";
+            TextReader text = File.OpenText(path);
+            var allLines = text.ReadToEnd().Split("\n");
+            int[] allIntegers = allLines.Select(int.Parse).ToArray();
+            int previousNumber = 0;
+            int amountOfIncresedNUmber = 0;
+
+            for(int i = 0; i < allIntegers.Length; i++)
+            {
+                if(previousNumber == 0)
+                {
+                    Console.WriteLine($"{allIntegers[i]} (N/A - no previous measurement)");
+                }
+                else if(allIntegers[i] > previousNumber)
+                {
+                    Console.WriteLine($"{allIntegers[i]} (increased)");
+                    amountOfIncresedNUmber++;
+                }
+                else
+                {
+                    Console.WriteLine($"{allIntegers[i]} (decreased)");
+                }
+                previousNumber = allIntegers[i];
+            }
+            Console.WriteLine(amountOfIncresedNUmber);
+        }
+    }
+}
---
+++ CodeOfAdvent2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace CodeOfAdvent2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:..\..\..\input.txt";
+            TextReader text = File.OpenText(path);
+            var allLines = text.ReadToEnd().Split("\n");
+            var instructionList = allLines.Select(x => x.Split(" ")).ToList();
+            var horizontalValue = 0;
+            var verticalValue = 0;
+            var total = 0;
+
+            foreach(var instruction in instructionList)
+            {
+                if (instruction[0] == "forward")
+                {
+                    horizontalValue += int.Parse(instruction[1]);
+                }
+                else if(instruction[0] == "down")
+                {
+                    verticalValue += int.Parse(instruction[1]);
+                }
+                else if(instruction[0] == "up")
+                {
+                    verticalValue -= int.Parse(instruction[1]);
+                }
+            }
+
+            total = horizontalValue * verticalValue;
+            Console.Write(total);
+        }
+    }
+}
